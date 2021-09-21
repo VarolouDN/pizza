f45270c5efc933b826e84acfc9b9682a4f7200a2
Denis Novosad <denisnovosad3@gmail.com>
2021-09-21 20:06:20 +0300

    21.09.21 20.05 BrowserRouter ,Link 4.3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import logoSvg from "../assets/img/pizza-logo.svg"
 import Button from "./Button";
+import {Link} from "react-router-dom"
+
 
 function Header(){
 
-return <div className="header">
+return (
+ 
+<div className="header">
 <div className="container">
+  <Link to="/">
   <div className="header__logo">
     <img width="38" src={logoSvg} alt="Pizza logo" />
     <div>
@@ -13,9 +18,10 @@ return <div className="header">
       <p>самая вкусная пицца во вселенной</p>
     </div>
   </div>
+  </Link>
   <div className="header__cart">
 
-
+<Link to="/cart">
 
       <Button className="button--cart">
 
@@ -53,7 +59,7 @@ return <div className="header">
       <span>3</span>
     </Button>
       
-
+</Link>
 
 
 
@@ -61,7 +67,7 @@ return <div className="header">
   </div>
 </div>
 </div>
-
+)
 }
 
 export default Header
