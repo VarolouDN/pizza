6bcf221be01228661dd728071849dad888352e26
Denis Novosad <denisnovosad3@gmail.com>
2021-10-31 18:47:17 +0200

    31.10.21 18.47

--- src/components/pages/Cart.jsx
+++ src/components/pages/Cart.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef } from 'react'
 import { useDispatch } from 'react-redux'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 import { setTotalCountPlusActionCreator,setTotalCountMinusActionCreator, setTotalPriceActionCreator, setCartLoaderActionCreator, setTotalPriceMinusActionCreator, deleteTotalPriceTotalCountActionCreator, changeTotalPriceTotalCountActionCreator } from '../../redux/actions/cartActionCreators'
 import { deleteFullPizzasCartActionCreator, deleteFullPizzasPizzaCartActionCreator, deletePizzasCartActionCreator, deletePizzasInfoPizzaCartActionCreator, deletePizzasPizzaCartActionCreator, setDeletePizzasInfoCartActionCreator, setFullPizzasCountActionCreator, setPizzasCountActionCreator, setPizzasInfoActionCreator } from '../../redux/actions/pizzaActionCreators'
 
@@ -276,13 +277,19 @@ let obj={
                 <span> Сумма заказа: <b>{state.totalPrice} ₽</b> </span>
               </div>
               <div className="cart__bottom-buttons">
-                <a href="/" className="button button--outline button--add go-back-btn">
+
+
+
+                <Link to="/" className="button button--outline button--add go-back-btn">
                   <svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M7 13L1 6.93015L6.86175 1" stroke="#D3D3D3" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
 </svg>
 
                   <span>Вернуться назад</span>
-                </a>
+                </Link>
+
+
+
                 <div className="button pay-btn">
                   <span>Оплатить сейчас</span>
                 </div>
